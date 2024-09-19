991eac7a254ecbb223e03a7c7527df7f885e801b
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-09-19 17:22:23 +0530

    make new pages

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import './Home.css'
+function Home(){
+return(
+    
+        <img src="\src\Image\ItsMe-removebg-preview.png" alt="" className='image-container'/>
+    
+
+)
+}
+export default Home
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Nav.css'
+function Navbar(){
+    return(
+        <nav className='navbar'>
+            <h1 className="name">Bishnupada Kamila</h1>
+            <a href="" className='content'>Home</a>
+            <a href="" className='content'>Projects</a>
+            <a href="" className='content'>Resume</a>
+            <a href="" className='content'>Contacts</a>
+        
+        </nav>
+    )
+}
+export default Navbar
