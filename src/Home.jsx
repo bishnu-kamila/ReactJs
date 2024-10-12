81d55d514cb5ca5612a161348f07b0e634a0b850
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-10-12 23:20:06 +0530

    Update Home.jsx
    Add some extra things

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,6 +6,7 @@ return(
                 <img src="\src\Image\ItsMe-removebg-preview.png" alt="" />
   
         <h1>Bishnupada Kamila From Odisha</h1>
+            <h1>Skilled in programming language </h1>
         </div>
     
 
