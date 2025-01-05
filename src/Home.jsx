17fa851fe5a77bfea08bef913a2ab2e5a3d3cbc0
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-01-05 21:43:36 +0530

    Update

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,13 @@ return(
     
         <div class="image-container">
                 <img src="\src\Image\ItsMe-removebg-preview.png" alt="" />
-  </div>
+        </div>,
+        <div >
+                Hello I am Bishnupada Kamila<br></br>
+                Skilled in Fulstack Development based on the text tack 
+                Java, SpringBoot, React, Angular, Node, Express, Mysql, Mongo DB
+                I am proficient in various programming languages and technology
+        </div>
     
 
 )
